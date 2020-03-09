d5aab9bbe74001b594019c22df2150c49fcb1d9b
Sylwia Duda <sylwia.agnieszka.duda@gmail.com>
2020-03-09 22:16:55 +0100

    fix parse authentication response

--- src/main/java/com/pl/orthography/rest/responseform/AuthenticationResponse.java
+++ src/main/java/com/pl/orthography/rest/responseform/AuthenticationResponse.java
@@ -1,12 +1,14 @@
 package com.pl.orthography.rest.responseform;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
+@Getter
 public class AuthenticationResponse implements Serializable {
-    private String userName;
+    private String user;
     private String email;
     private String token;
 }
